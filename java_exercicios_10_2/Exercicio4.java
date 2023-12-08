92e1fae1639dd763e45c06583420a8b9fbfa2315
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2023-12-08 15:51:39 -0300

    Exercicios do dia 08/12

---
+++ java_exercicios_10_2/Exercicio4.java
@@ -0,0 +1,10 @@
+package java_exercicios_10_2;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo");
+	}
+
+}
